febc91643e020b5c9ff490db1f61d3a01a91ee81
danishdrx <154459115+danishdrx@users.noreply.github.com>
2023-12-29 21:01:26 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     for(int i=0;i<=n;i++){         printf("%d",(n+1)*n/2);         break;     } }

--- Sum_of_first_N_natural_Numbers.c
+++ Sum_of_first_N_natural_Numbers.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main(){
-    int n,i;
+    int n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        printf("%d ",(n+1)*n/2);
+    for(int i=0;i<=n;i++){
+        printf("%d",(n+1)*n/2);
         break;
     }
-    
 }
